aada115c2edc58c124b898c77024174923670137
Asitha Nanayakkara <daun07@gmail.com>
2023-06-23 17:21:30 +0530

    Replace Vite Config with Config.js to support Choreo web apps
    - Replace Vite config with a simple config.js to provide the configs
      for different environments in Choreo. Config values will be popluated
      to the Window object for the web app to read. On each Choreo environment
      there will be a different config.js file mounted
    
    - Remove unwanted files from code

---
+++ cloud-native-app-developer/reading-list-front-end/config.js
@@ -0,0 +1,6 @@
+window.config = {
+  redirectUrl: "http://localhost:5173",
+  asgardeoClientId: "BTKr9f3xg6CmYiylm7jsIiTku0ka",
+  asgardeoBaseUrl: "https://api.asgardeo.io/t/msteam",
+  choreoApiUrl: "https://6de500af-4608-46ca-944b-9f9810807bda-dev.e1-us-east-azure.choreoapis.dev/pera/reading-list/readinglist-a57/1.0.0",
+};
